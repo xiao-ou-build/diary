796df707f8fb24ac6832fefaf52e0e260809b12f
Jian Lin <jianlin1@microsoft.com>
2026-04-09 19:30:15 +0800

    chore: migrate to xiao-ou-build GitHub account

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://linjianbjfu.github.io",
+	site: "https://xiao-ou-build.github.io",
 	base: "/diary/",
 	trailingSlash: "always",
 	integrations: [
--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/linjianbjfu/diary", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/xiao-ou-build/diary", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -60,7 +60,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/linjianbjfu",
+			url: "https://github.com/xiao-ou-build",
 		},
 	],
 };
